049478ae5a58b19853bb536fe82827179ba8f74b
Yusuf Isaiah Ifeanyichukwu <isawhizi@gmail.com>
2026-05-22 18:46:23 +0100

    week2-node-express

--- serve.js
+++ serve.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+const path = require("path");
 
+
+//Json Parser middleware
+app.use(express.json());
+//Serve Static file
+app.use(express.static(path.join(__dirname, "public")));
+//Logger middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} at ${new Date()}`);
+    next();
+});
+
+//Root route
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("My Week 2 API!");
+});
+//Route to serve the HTML file
+app.get("/html", (req, res) => {
+    res.sendFile(path.join(__dirname, "Intro.html"));
+});
+//User route for creating a new user
+app.post("/user", (req, res) => {
+    const { name, email } = req.body;
+    if (!name || !email) return res.status(400).send("Name and email are required.");
+    res.send(`Hello, ${name}!`);
+    //res.status(201).json({ message: `Hello, ${name}!` });
+});
+//User route for fetching user profile
+app.get("/user/:id", (req, res) => {
+    const userId = req.params.id;
+    res.send(`User ${userId} profile.`);
 });
 
 app.listen(port, () => {
